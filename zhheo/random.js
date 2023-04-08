a5b19e7b52c51dcfe2ff50e2f96b18357c4d0a3d
Skytyun <2909214343@qq.com>
2023-04-08 22:52:09 +0800

    Site updated: 2023-04-08 22:52:09

--- zhheo/random.js
+++ zhheo/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/ebdb339d.html","posts/52c9d35.html","posts/2013454d.html","posts/b3d7b7de.html","posts/61d7c041.html","posts/d6d9fc07.html","posts/e6a2025.html","posts/43748377.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/52c9d35.html","posts/ebdb339d.html","posts/61d7c041.html","posts/d6d9fc07.html","posts/b3d7b7de.html","posts/2013454d.html","posts/43748377.html","posts/e6a2025.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
