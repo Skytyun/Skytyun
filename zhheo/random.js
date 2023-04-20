7799ba16058eda3daf85a71a45ea685a4b4c3c5f
Skytyun <2909214343@qq.com>
2023-04-20 13:01:43 +0800

    Site updated: 2023-04-20 13:01:42

--- zhheo/random.js
+++ zhheo/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/52c9d35.html","posts/e6a2025.html","posts/61d7c041.html","posts/43748377.html","posts/2013454d.html","posts/d6d9fc07.html","posts/b3d7b7de.html","posts/ebdb339d.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/e6a2025.html","posts/2013454d.html","posts/43748377.html","posts/52c9d35.html","posts/61d7c041.html","posts/b3d7b7de.html","posts/ebdb339d.html","posts/d6d9fc07.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
